ea83e533cbeb1c1a78ef214927f5a625949661e9
Arnav Choudhury <66573183+8coolguy@users.noreply.github.com>
2024-05-21 23:27:16 -0700

    Init

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-# Getting Started with Create React App
+# Raft Demo Frontend
+THis project is supposed to be a demonstration of a file revision system using the RAFT algorithm is multiple nodes. A user will be able to selct which node to connect and can edit a file and commit it to the system. The distruibted system will ensure consistency and consesus between the nodes. On conflict the user will be able to select which changes that they want to keep. An exmaple of how it should look like.
+
+![Mockup](https://imgur.com/a/K68tsSI)
+
+
+
+
+
+## Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="status" element={<Status />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
+}
+function Layout(){
+  return (<>  <Link to="/">Home</Link> <Link to="/status">Status</Link> <Outlet/> </>)
+}
+function Home(){
+  return (<div>Hello Home</div>)
+}
+function Status(){
+  return (<div>Hello Status</div>)
 }
 
 export default App;
